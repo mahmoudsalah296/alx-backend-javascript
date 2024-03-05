050ba97b91d1cd3a501160b80df794319bf87952
Mahmoud Salah <ms685116@gmail.com>
2024-03-05 18:41:23 +0200

    promise.allSettled

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,11 +5,9 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]).then((results) =>
-    results.map((result) => ({
-      status: result.status,
-      value:
+  ]).then((results) => results.map((result) => ({
+    status: result.status,
+    value:
         result.status === 'fulfilled' ? result.value : String(result.reason),
-    }))
-  );
+  })));
 }
