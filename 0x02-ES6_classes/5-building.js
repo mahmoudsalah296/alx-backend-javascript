a9526674e7e2ddc7aadafb20fbcd31f44b2d6b9e
Mahmoud Salah <ms685116@gmail.com>
2024-03-07 20:04:17 +0200

    abstract methods

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,10 +1,12 @@
 export default class Building {
   constructor(sqft) {
     this.sqft = sqft;
-    if (typeof this.evacuationWarningMessage !== 'function') {
-      throw new Error(
-        'Class extending Building must override evacuationWarningMessage'
-      );
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error(
+          'Class extending Building must override evacuationWarningMessage',
+        );
+      }
     }
   }
 
