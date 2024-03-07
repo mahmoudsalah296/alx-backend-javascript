6aeff016a0b68c43213f77b9c4aa3925e9c6f084
Mahmoud Salah <ms685116@gmail.com>
2024-03-07 20:40:40 +0200

    number representation of an object

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -4,13 +4,13 @@ export default class HolbertonClass {
     this._location = location;
   }
 
-  // number representation of an object
-  valueOf() {
-    return this._size;
-  }
-
-  // string representation of an object
-  get [Symbol.toStringTag]() {
-    return this._location;
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this._size;
+    }
+    if (hint === 'string') {
+      return this._location;
+    }
+    return null;
   }
 }
