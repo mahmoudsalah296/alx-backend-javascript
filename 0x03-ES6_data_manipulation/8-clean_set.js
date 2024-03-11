5dfdc443dddaaa2028dad3c9b1d44ceaf7de7431
Mahmoud Salah <ms685116@gmail.com>
2024-03-11 14:34:58 +0200

    clean set

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,11 @@
+export default function cleanSet(set, str) {
+  const out = [];
+
+  if (!set || !str || !(set instanceof Set) || typeof str !== 'string') {
+    return '';
+  }
+  set.forEach((val) => {
+    if (val.startsWith(str)) out.push(val.slice(str.length));
+  });
+  return out.join('-');
+}
