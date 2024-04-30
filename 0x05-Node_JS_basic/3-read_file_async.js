8cf5d9d6a69a03112dc7b83a8ed8a82b440975da
Mahmoud Salah <ms685116@gmail.com>
2024-04-30 22:30:22 +0300

    all tasks

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 /**
  * Counts the students in a CSV data file.
  * @param {String} dataPath The path to the CSV data file.
+ * @author Mahmoud Salah
  */
 const countStudents = (dataPath) =>
   new Promise((resolve, reject) => {
