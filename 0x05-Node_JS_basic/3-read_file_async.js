3baacbbfd67904441855ec591a5e263bc8043c87
Mahmoud Salah <ms685116@gmail.com>
2024-04-30 22:23:44 +0300

    all tasks

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,22 +1,22 @@
-const fs = require("fs");
+const fs = require('fs');
 /**
  * Counts the students in a CSV data file.
  * @param {String} dataPath The path to the CSV data file.
  */
 const countStudents = (dataPath) =>
   new Promise((resolve, reject) => {
-    fs.readFile(dataPath, "utf-8", (err, data) => {
+    fs.readFile(dataPath, 'utf-8', (err, data) => {
       if (err) {
-        reject(new Error("Cannot load the database"));
+        reject(new Error('Cannot load the database'));
       }
       if (data) {
-        const fileLines = data.toString("utf-8").trim().split("\n");
+        const fileLines = data.toString('utf-8').trim().split('\n');
         const studentGroups = {};
-        const dbFieldNames = fileLines[0].split(",");
+        const dbFieldNames = fileLines[0].split(',');
         const studentPropNames = dbFieldNames.slice(0, dbFieldNames.length - 1);
 
         for (const line of fileLines.slice(1)) {
-          const studentRecord = line.split(",");
+          const studentRecord = line.split(',');
           const studentPropValues = studentRecord.slice(
             0,
             studentRecord.length - 1
@@ -27,7 +27,7 @@ const countStudents = (dataPath) =>
           }
           const studentEntries = studentPropNames.map((propName, idx) => [
             propName,
-            studentPropValues[idx],
+            studentPropValues[idx]
           ]);
           studentGroups[field].push(Object.fromEntries(studentEntries));
         }
@@ -39,7 +39,7 @@ const countStudents = (dataPath) =>
         for (const [field, group] of Object.entries(studentGroups)) {
           const studentNames = group
             .map((student) => student.firstname)
-            .join(", ");
+            .join(', ');
           console.log(
             `Number of students in ${field}: ${group.length}. List: ${studentNames}`
           );
