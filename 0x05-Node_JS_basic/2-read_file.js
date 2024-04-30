b7afd11be70023350b619f22d16b754b6ae6afa6
Mahmoud Salah <ms685116@gmail.com>
2024-04-30 22:35:53 +0300

    all tasks

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 /**
  * Counts the students in a CSV data file.
  * @param {String} dataPath The path to the CSV data file.
+ * @author Mahmoud Salah
  */
 const countStudents = (dataPath) => {
   if (!fs.existsSync(dataPath)) {
@@ -27,22 +28,18 @@ const countStudents = (dataPath) => {
     if (!Object.keys(studentGroups).includes(field)) {
       studentGroups[field] = [];
     }
-    const studentEntries = studentPropNames.map((propName, idx) => [
-      propName,
-      studentPropValues[idx]
-    ]);
+    const studentEntries = studentPropNames
+      .map((propName, idx) => [propName, studentPropValues[idx]]);
     studentGroups[field].push(Object.fromEntries(studentEntries));
   }
 
-  const totalStudents = Object.values(studentGroups).reduce(
-    (pre, cur) => (pre || []).length + cur.length
-  );
+  const totalStudents = Object
+    .values(studentGroups)
+    .reduce((pre, cur) => (pre || []).length + cur.length);
   console.log(`Number of students: ${totalStudents}`);
   for (const [field, group] of Object.entries(studentGroups)) {
     const studentNames = group.map((student) => student.firstname).join(', ');
-    console.log(
-      `Number of students in ${field}: ${group.length}. List: ${studentNames}`
-    );
+    console.log(`Number of students in ${field}: ${group.length}. List: ${studentNames}`);
   }
 };
 
